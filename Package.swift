e7c543b0efafba82cd737db3a2dde28dab065f07
Stuart A. Malone <samalone@llamagraphics.com>
2023-04-17 13:58:33 -0400

    Add JWT

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/samalone/GeoKit.git", from: "2.0.0"),
         .package(url: "https://github.com/samalone/vapor-lingua-franca.git", branch: "main"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,7 +29,8 @@ let package = Package(
             name: "CourseKit",
             dependencies: [
                 .product(name: "GeoKit", package: "geokit"),
-                .product(name: "LinguaFranca", package: "vapor-lingua-franca")
+                .product(name: "LinguaFranca", package: "vapor-lingua-franca"),
+                .product(name: "JWT", package: "jwt"),
             ]),
         .testTarget(
             name: "CourseKitTests",
